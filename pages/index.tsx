fac23ad0eb247e6f11a5f66dc5669c7d1d5b0c16
Joshua Jourdain <yahwehprovides@Joshuas-iMac.local>
2021-09-06 12:17:09 -0400

    test connection with typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -57,7 +57,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
+          Poweredsassd by{" "}
           <img
             src="https://yt3.ggpht.com/ytc/AKedOLSU2P3VVbKEkoCPaJQG9i27gF2Ou1fIzqne3Cl-=s176-c-k-c0x00ffffff-no-rj"
             alt="Vercel Logo"
